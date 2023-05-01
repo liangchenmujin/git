43f5fc3b4478fd4be09d0b174aa0c67e5907af57
liangchenmujin <132043815+liangchenmujin@users.noreply.github.com>
2023-05-01 16:01:24 +0800

    Update GitTest.java

--- src/main/java/com/example/GitTest.java
+++ src/main/java/com/example/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello git 2");
         System.out.println("hot-fix hello git 1");
+        System.out.println("pull test");
     }
 }
